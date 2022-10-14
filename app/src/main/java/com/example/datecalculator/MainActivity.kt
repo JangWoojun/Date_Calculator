eaa3586b25c6e44ce4a7be355868d76121fc0506
JangWoojun <woojun0107@naver.com>
2022-10-14 21:28:59 +0900

    2022/10/14 초 계산 추가

--- app/src/main/java/com/example/datecalculator/MainActivity.kt
+++ app/src/main/java/com/example/datecalculator/MainActivity.kt
@@ -58,18 +58,18 @@ class MainActivity : AppCompatActivity() {
                         val cDTime = currentDate.time/86400000
                         val cHTime = currentDate.time/3600000
                         val cMTime = currentDate.time/60000
-
+                        val cSTime = currentDate.time/1000
 
                         val endDTime = cDTime - dTime
                         val endHTime = cHTime - dTime
                         val endMTime = cMTime - mTime
-
+                        val endSTime = cSTime - dTime
 
 
                         timeTextD?.text = endDTime.toString()+" Day"
                         timeTextH?.text = endHTime.toString()+" Hour"
                         timeTextM?.text = endMTime.toString()+" Minute"
-
+                        timeTextH?.text = endSTime.toString()+" Second"
 
                     }
                 }

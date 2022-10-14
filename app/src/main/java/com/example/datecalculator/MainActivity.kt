1d38f9661c77c06a3170f73c1fe6f93d56b05bc4
JangWoojun <woojun0107@naver.com>
2022-10-14 20:21:15 +0900

    2022/10/14 내가 태어난 지 몇분이 지났는 지 알려주는 기능 완성

--- app/src/main/java/com/example/datecalculator/MainActivity.kt
+++ app/src/main/java/com/example/datecalculator/MainActivity.kt
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.DatePicker
+import android.widget.TextView
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var dateView : TextView? = null
+    private var timeText : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dateView = findViewById(R.id.date)
+        timeText = findViewById(R.id.timeText)
 
         val dateBtn = findViewById<Button>(R.id.dateBtn)
         dateBtn.setOnClickListener { view ->
             view
             datePicker()
+
         }
 
     }
@@ -25,12 +33,31 @@ class MainActivity : AppCompatActivity() {
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth->
 
-        DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth-> },
+                dateView?.text = "$year/${month + 1}/$dayOfMonth"
+
+                val sdf = SimpleDateFormat("yyyy/MM/dd", Locale.KOREA)
+                val theDate = sdf.parse("$year/${month + 1}/$dayOfMonth")
+
+                theDate.let { // let 으로 오류 방지
+                    val sMTime = theDate.time / 60000 // 선택한 날짜 구하기
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis())) // 현재 날짜 구하기
+
+                    currentDate.let {
+                        val cMTime = currentDate.time/60000
+                        val endTime = cMTime - sMTime
+                        timeText?.text = endTime.toString()
+                    }
+                }
+
+            },
             year,
             month,
             day
-        ).show()
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000 //과거 시간만 설정할 수 있게 변경
+        dpd.show()
     }
 }

f022c43e5765f71c339f2982b9b4942f604d4f08
JangWoojun <woojun0107@naver.com>
2022-10-14 19:49:18 +0900

    2022/10/14 날짜 선택 추가

--- app/src/main/java/com/example/datecalculator/MainActivity.kt
+++ app/src/main/java/com/example/datecalculator/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.datecalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dateBtn = findViewById<Button>(R.id.dateBtn)
+        dateBtn.setOnClickListener { view ->
+            view
+            datePicker()
+        }
+
+    }
+    fun datePicker(){
+
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth-> },
+            year,
+            month,
+            day
+        ).show()
     }
 }
